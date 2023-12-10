5d0db9d4e8e04760b763bd52198a98a776af36a2
Rohan Rudhiyal <128076799+RohanRudhiyal@users.noreply.github.com>
2023-12-10 14:35:12 +0530

    Add files via upload

---
+++ bool.js
@@ -0,0 +1,16 @@
+let arr =["rohan","xyz","151802"];
+
+document.getElementById('data').innerHTML=arr[0];
+document.getElementById('data2').innerHTML=arr[1];
+document.getElementById('data3').innerHTML=arr[2];
+
+let obj ={
+    name :"Arpit" ,
+    email : "AbC" ,
+    id : "123",
+}
+
+document.getElementById('data4').innerHTML=obj.name;
+document.getElementById('data5').innerHTML=obj.email;
+document.getElementById('data6').innerHTML=obj.id;
+
